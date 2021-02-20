f1c68f739ea63aec254b2aba40d2d7a2157e1bdc
Wesley Fuchter <wesley.fuchter@gmail.com>
2021-02-20 16:59:12 -0300

    Create react app + material + graphql + router.

---
+++ src/Portuguese.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Button } from '@material-ui/core';
+
+function Portuguese() {
+  return <Button color="primary">Olá Mundo!</Button>;
+}
+
+export default Portuguese;
