67ea5457792934afea80ee1e8d0c98f6218b2e47
Aaron-Goldman <aaron.goldman@moduscreate.com>
2021-03-02 15:03:42 -0500

    class to pure function

--- src/PrivateRoute.js
+++ src/PrivateRoute.js
@@ -2,23 +2,40 @@ import React from 'react';
 import {
   Route,
   Redirect,
+  useLocation,
 } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import { LOGIN_PATH } from './paths';
 
-export default class PrivateRoute extends Route {
-  render() {
-    const loggedIn = false;
-    return (
-      loggedIn ? (
-        this.props.children
-      ) : (
-        <Redirect
-          to={{
-            pathname: LOGIN_PATH,
-            state: { from: this.props.location },
-          }}
-        />
-      )
-    );
-  }
+function PrivateRoute(props) {
+  const loggedIn = false;
+  const { path, exact, component } = props;
+  const location = useLocation();
+
+  return (
+    loggedIn ? (
+      <Route path={path} exact={exact} component={component} />
+    ) : (
+      <Redirect
+        to={{
+          pathname: LOGIN_PATH,
+          state: { from: location },
+        }}
+      />
+    )
+  );
 }
+
+PrivateRoute.defaultProps = {
+  path: '/',
+  exact: false,
+  component: undefined,
+};
+
+PrivateRoute.propTypes = {
+  path: PropTypes.string,
+  exact: PropTypes.bool,
+  component: PropTypes.element,
+};
+
+export default PrivateRoute;
