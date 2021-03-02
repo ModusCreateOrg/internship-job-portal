64ab47081715c4c29b71b09a704467aebd72ec89
Aaron-Goldman <aaron.goldman@moduscreate.com>
2021-03-02 13:57:39 -0500

    class to pure function

--- src/Login/index.js
+++ src/Login/index.js
@@ -1,81 +1,68 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import Card from '@material-ui/core/Card';
 import { Link } from 'react-router-dom';
 
-class Login extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      username: '',
-      password: '',
-    };
+function Login() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
 
-    this.handleInputChange = this.handleInputChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
+  const handleUsernameInput = (event) => {
+    setUsername(event.target.value);
+  };
+  const handlePasswordInput = (event) => {
+    setPassword(event.target.value);
+  };
 
-  handleInputChange(event) {
-    this.setState({
-      [event.target.name]: event.target.value,
-    });
-  }
-
-  handleSubmit(event) {
-    const { username, password } = this.state;
+  const handleSubmit = () => {
     console.log(username, password);
-    event.preventDefault();
-  }
+  };
 
-  render() {
-    const { username, password } = this.state;
-    return (
-      <Card style={{
-        boxSizing: 'border-box',
-        maxWidth: '400px',
-        margin: '1em auto',
-        alignItems: 'center',
-      }}
+  return (
+    <Card style={{
+      boxSizing: 'border-box',
+      maxWidth: '400px',
+      margin: '1em auto',
+      alignItems: 'center',
+    }}
+    >
+      <form
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+        }}
+        onSubmit={handleSubmit}
       >
-        <form
-          style={{
-            display: 'flex',
-            flexDirection: 'column',
-          }}
-          onSubmit={this.handleSubmit}
+        <TextField
+          id="username-field"
+          label="Username"
+          autoComplete="username"
+          value={username}
+          onChange={handleUsernameInput}
+        />
+        <TextField
+          id="password-field"
+          label="Password"
+          type="password"
+          autoComplete="current-password"
+          name="password"
+          value={password}
+          onChange={handlePasswordInput}
+        />
+        <Button
+          variant="outlined"
+          color="primary"
+          type="submit"
         >
-          <TextField
-            id="username-field"
-            label="Username"
-            autoComplete="username"
-            name="username"
-            value={username}
-            onChange={this.handleInputChange}
-          />
-          <TextField
-            id="password-field"
-            label="Password"
-            type="password"
-            autoComplete="current-password"
-            name="password"
-            value={password}
-            onChange={this.handleInputChange}
-          />
-          <Button
-            variant="outlined"
-            color="primary"
-            type="submit"
-          >
-            Login
-          </Button>
-        </form>
-        <Button variant="text" color="default" component={Link} to="/register">
-          Register
+          Login
         </Button>
-      </Card>
-    );
-  }
+      </form>
+      <Button variant="text" color="default" component={Link} to="/register">
+        Register
+      </Button>
+    </Card>
+  );
 }
 
 export default Login;
--- src/Register/index.js
+++ src/Register/index.js
@@ -1,105 +1,85 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import Card from '@material-ui/core/Card';
 
-class Register extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      username: '',
-      firstName: '',
-      lastName: '',
-      password: '',
-    };
+function Register() {
+  const [username, setUsername] = useState('');
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [password, setPassword] = useState('');
 
-    this.handleInputChange = this.handleInputChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
+  const handleUsernameInput = (event) => {
+    setUsername(event.target.value);
+  };
+  const handleFirstNameInput = (event) => {
+    setFirstName(event.target.value);
+  };
+  const handleLastNameInput = (event) => {
+    setLastName(event.target.value);
+  };
+  const handlePasswordInput = (event) => {
+    setPassword(event.target.value);
+  };
 
-  handleInputChange(event) {
-    this.setState({
-      [event.target.name]: event.target.value,
-    });
-  }
-
-  handleSubmit(event) {
-    const {
-      username,
-      firstName,
-      lastName,
-      password,
-    } = this.state;
+  const handleSubmit = () => {
     console.log(username, firstName, lastName, password);
-    event.preventDefault();
-  }
+  };
 
-  render() {
-    const {
-      username,
-      firstName,
-      lastName,
-      password,
-    } = this.state;
-    return (
-      <Card style={{
-        boxSizing: 'border-box',
-        maxWidth: '400px',
-        margin: '1em auto',
-        alignItems: 'center',
-      }}
+  return (
+    <Card style={{
+      boxSizing: 'border-box',
+      maxWidth: '400px',
+      margin: '1em auto',
+      alignItems: 'center',
+    }}
+    >
+      <form
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+        }}
+        onSubmit={handleSubmit}
       >
-        <form
-          style={{
-            display: 'flex',
-            flexDirection: 'column',
-          }}
-          onSubmit={this.handleSubmit}
+        <TextField
+          id="username-field"
+          label="Username"
+          autoComplete="username"
+          value={username}
+          onChange={handleUsernameInput}
+        />
+        <TextField
+          id="firstName-field"
+          label="First Name"
+          autoComplete="given-name"
+          value={firstName}
+          onChange={handleFirstNameInput}
+        />
+        <TextField
+          id="lastName-field"
+          label="Last Name"
+          autoComplete="family-name"
+          value={lastName}
+          onChange={handleLastNameInput}
+        />
+        <TextField
+          id="password-field"
+          label="Password"
+          type="password"
+          autoComplete="new-password"
+          value={password}
+          onChange={handlePasswordInput}
+        />
+        <Button
+          variant="outlined"
+          color="primary"
+          type="submit"
         >
-          <TextField
-            id="username-field"
-            label="Username"
-            autoComplete="username"
-            value={username}
-            name="username"
-            onChange={this.handleInputChange}
-          />
-          <TextField
-            id="firstName-field"
-            label="First Name"
-            autoComplete="given-name"
-            value={firstName}
-            name="firstName"
-            onChange={this.handleInputChange}
-          />
-          <TextField
-            id="lastName-field"
-            label="Last Name"
-            autoComplete="family-name"
-            value={lastName}
-            name="lastName"
-            onChange={this.handleInputChange}
-          />
-          <TextField
-            id="password-field"
-            label="Password"
-            type="password"
-            autoComplete="new-password"
-            value={password}
-            name="password"
-            onChange={this.handleInputChange}
-          />
-          <Button
-            variant="outlined"
-            color="primary"
-            type="submit"
-          >
-            Register
-          </Button>
-        </form>
-      </Card>
-    );
-  }
+          Register
+        </Button>
+      </form>
+    </Card>
+  );
 }
 
 export default Register;
